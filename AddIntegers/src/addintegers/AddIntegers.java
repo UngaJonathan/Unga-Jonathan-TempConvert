e20b762b240677599ed4f6275c4d19898346eba1
Jonathan Unga <38771415+jonunga@users.noreply.github.com>
2018-09-29 07:47:57 -0700

    09/29/2018 - created Add integers program

---
+++ AddIntegers/src/addintegers/AddIntegers.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package addintegers;
+
+/**
+ *
+ * @author Jonathan
+ */
+import java.util.Scanner;
+
+public class AddIntegers 
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        // TODO code application logic here
+        int n1, n2, sum;
+        Scanner keyboard = new Scanner(System.in);
+        
+        System.out.println("Enter your first integer value: ");
+        n1 = keyboard.nextInt();
+        
+        System.out.println("Enter your second integer value: ");
+        n2 = keyboard.nextInt();
+        
+        sum = n1 + n2;
+        System.out.println("The sum of you two numbers is " + sum);
+    }
+    
+}
