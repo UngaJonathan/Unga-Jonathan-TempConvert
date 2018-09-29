07659ca148e6c24338b69669e9ad417cc8b29839
Jonathan Unga <38771415+jonunga@users.noreply.github.com>
2018-09-29 09:36:23 -0700

    09/29/2018 - created C to F conversion program with name change

---
+++ TempConvert/src/tempconvert/TempConvert.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tempconvert;
+
+/**
+ *
+ * @author Jonathan
+ */
+import java.util.Scanner;
+
+public class TempConvert 
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        // TODO code application logic here
+        double tempC, tempF;
+        Scanner keyboard = new Scanner(System.in);
+        
+        System.out.println("Please enter temperature in Celsius to be converted to Fahrenheit: ");
+        tempC = keyboard.nextDouble();
+        
+        tempF = (9.0/5.0) * tempC + 32.0;
+        System.out.println("The temperature is " + tempF + " Fahrenheit!");
+    }
+    
+}
